51053b7b33ffb14455b171fd31258fdb678f7b28
devzhk <devzhk@gmail.com>
2024-02-24 17:46:16 +0000

    optimize exp folder structure

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     torch.backends.cudnn.benchmark = True
     parser = ArgumentParser()
     parser.add_argument('--config', type=str, default='configs/classificaiton/vae-feat.yaml')
-    parser.add_argument('--exp_dir', type=str, default='exps/clf')
+    parser.add_argument('--exp_dir', type=str, default='exps')
     parser.add_argument('--test', action='store_true', help='Test the model.')
     parser.add_argument('--ckpt', type=str, default=None)
     parser.add_argument('--use_wandb', action='store_true', help='Use wandb to log the experiment.')
@@ -34,11 +34,19 @@ if __name__ == '__main__':
     else:
         raise ValueError(f'Invalid task name: {config.model.task_name}')
 
-    exp_dir = os.path.join(args.exp_dir, f'{config.model.task_name}_{config.model.name}_'
+    root_dir = os.path.join(args.exp_dir, config.model.task_name)
+    os.makedirs(root_dir, exist_ok=True)
+    exp_dir = os.path.join(root_dir, f'{config.model.name}_'
                            f'seq{config.model.seq_len}_label{config.model.label_len}_pred{config.model.pred_len}_'
                            f'bs{config.train.batch_size}_dm{config.model.d_model}_{config.log.tag}')
-    if args.test:        
-        exp.eval(exp_dir)
+    if args.test:
+        if config.model.task_name == 'classification':     
+            exp.eval(exp_dir)
+        else:
+            for subject in config.data.test_subjects:
+                for act in config.data.test_acts:
+                    print(act)
+                    exp.eval(exp_dir, subject, act)
     else:
         exp.train(exp_dir, eval=True, use_wandb=args.use_wandb)
 
